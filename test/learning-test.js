45fb3ffe1dbaadc106ba2929874c381dc5ff0c2a
Torstein Nicolaysen <torstein.nicolaysen@bekk.no>
2011-10-10 19:38:02 +0200

    Added initial structure
    Contains placeholder files
    
    Contains the start for learning tests

---
+++ test/learning-test.js
@@ -0,0 +1,33 @@
+module("42");
+
+// Oppgave 1:
+// Skriv en test som verifiserer at 1 + 41 = 42 ved hjelp av ok()
+test("is ok", function() {
+    // Arrange
+    // Act
+    // Assert
+});
+
+// Oppgave 2
+// Skriv en test som verifiserer at 1 + 41 = 42 ved hjelp av equals()
+
+
+// Oppgave 3:
+// Lag ny modul kalt 'Objects'
+// INFO: Objekter og arrays vil oppføre seg på samme måte under test,
+//       med det unntaket at arrays aksesseres med syntaksen array[index]
+
+
+// Oppgave 4: 
+// Sammenlign de to objektene under ved hjelp av same()
+
+var myCoins = {notes_100: 1};
+var yourCoins = {notes_100: 1};
+
+
+// Oppgave 5: 
+// Refaktorer myCoins og yourCoins til å initialiseres i setup i 'Objects'-modulen
+// Se på dokumentasjonen til module()
+
+// Oppgave 6: 
+// Sammenlign myCoins og yourCoins med equals()
